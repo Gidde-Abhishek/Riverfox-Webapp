3ab4c7c15edbeb1b3325061900bddb7b3e928063
Abhishek Gidde <abhishekct12@gmail.com>
2021-12-12 14:43:49 +0530

    init

---
+++ res/js/script.js
@@ -0,0 +1,5 @@
+var whatsappMe = () => {
+  window.open(
+    "https://web.whatsapp.com/send?phone=919619067383&text=Hi%2C+I+would+like+to+know+more+about+how+you+can+help+my+business+grow.+%F0%9F%98%84"
+  );
+};
